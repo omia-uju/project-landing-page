01119188881ea4fc020b4b05ec4b855541e4803c
Omia George <georgeomia517@gmail.com>
2024-10-04 13:15:18 +0300

    Add files via upload

--- Note.java
+++ Note.java
@@ -1,10 +1,27 @@
-package note;
-
+package controlflow;
+import java.util.Scanner;
 public class Note {
 
 	public static void main(String[] args) {
-	int i=1;
-  do {
-	  System.out.println(i);
-	  i++;
-  }while(i<=10);}}
+		 Scanner scanner = new Scanner(System.in);
+	        int number;
+	        // this loop will run until the user enters a postive number
+	        do {
+	            System.out.print("Enter a number (postive to quit): ");
+	            number = scanner.nextInt();
+	            if (number < 0) {
+	                System.out.println("You entered: " + number);
+	            } else {
+	                System.out.println("Exiting...");
+	            }
+	        } while (number < 0);
+	        scanner.close();
+
+		
+           
+		 }
+	}
+           
+	
+
+  
